ca61a12d8e011bdeafa3fb2fa67bc96f03048fbe
Erwan Demairy <Erwan.Demairy@inria.fr>
2024-11-05 14:13:53 +0100

    signature ok.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,29 +3,47 @@
  */
 
 plugins {
+    `java`
     `java-library`
     `maven-publish`
+    `signing` // pour signer les artefacts, requis pour Maven Central
 }
 
-repositories {
-    mavenLocal()
-    maven {
-        name = "ossrh"
-        url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-        credentials {
-            username = findProperty("ossrhUsername") ?: ""
-            password = findProperty("ossrhPassword") ?: ""
+
+publishing {
+    publications.create<MavenPublication>("test_maven") {
+        from(components["java"])
+    }
+
+    repositories {
+        mavenLocal()
+        maven {
+            name = "ossrh"
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                val user = project.properties["ossrhUsername"]
+                val password = project.properties["ossrhPassword"]
+            }
         }
     }
 }
+signing {
+    sign(publishing.publications["test_maven"])
+}
+
+
+
+
+
+
+
+
+
+
 
 group = "io.github.edemairy"
 version = "0.0.1-SNAPSHOT"
 description = "Maven core"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
-publishing {
-    publications.create<MavenPublication>("maven") {
-        from(components["java"])
-    }
-}
+
